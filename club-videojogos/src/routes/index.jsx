488fa813ed5b0ad230cb7efcb8825c1569fb786d
Grimidk <thegrimidk@gmail.com>
2024-03-09 22:06:16 -0400

    firebase fix again

--- club-videojogos/src/routes/index.jsx
+++ club-videojogos/src/routes/index.jsx
@@ -1,13 +1,7 @@
 export default function Index() {
     return (
       <p id="zero-state">
-        This is a demo for React Router.
-        <br />
-        Check out{" "}
-        <a href="https://reactrouter.com">
-          the docs at reactrouter.com
-        </a>
-        .
+        This is the Dashboard for the Group page.
       </p>
     );
   }
